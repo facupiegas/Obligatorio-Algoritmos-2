dad466e897ece5bfa5ad92c3a5136f47eb5bbaca
Facundo <facundo.piegas@gmail.com>
2018-10-27 15:55:34 -0300

    Creacion

---
+++ Obligatorio Algoritmos 2/src/obligatorio/algoritmos/pkg2/ObligatorioAlgoritmos2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package obligatorio.algoritmos.pkg2;
+
+/**
+ *
+ * @author Facundo
+ */
+public class ObligatorioAlgoritmos2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
